8ae04cc21b6568e97c087a76603adaf0003a29a2
Hamas Ur Rehman <47780362+Hamas-ur-Rehman@users.noreply.github.com>
2021-02-03 10:30:28 +0500

    Master

---
+++ New_installtion.ps1
@@ -0,0 +1,24 @@
+Set-ExecutionPolicy AllSigned or Set-ExecutionPolicy Bypass -Scope Process.
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install flashplayerplugin -y
+choco install googlechrome -y
+choco install chocolatey-core.extension -y
+choco install jre8 -y
+choco install 7zip.install -y
+choco install vcredist140 -y
+choco install notepadplusplus.install -y
+choco install vlc -y
+choco install git.install -y
+choco install python3 -y
+choco install dotnetfx -y
+choco install winrar -y
+choco install vscode -y
+choco install zoom -y
+choco install audacity -y
+choco install intellijidea-community
+choco install steam
+choco install dart-sdk -y
+choco install cpu-z.install -y
+choco install whatsapp -y
+choco install androidstudio
+choco install unity
